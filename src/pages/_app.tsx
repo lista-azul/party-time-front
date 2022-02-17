f3aafc98747bd6f8dfe47a3afbbef7467f6657f7
beetlejuice96 <dilanbernbae043@gmail.com>
2022-02-17 16:22:51 -0300

    set material ui with next js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import PropTypes from "prop-types";
+import createEmotionCache from "src/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+import Head from "next/head";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "src/styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props: any) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
