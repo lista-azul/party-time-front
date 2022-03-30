ca4d8870d1ed20a5a991f90c333fba74b667cc54
beetlejuice96 <dilanbernbae043@gmail.com>
2022-03-30 15:46:36 -0300

    #8 add event service

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,21 @@
-import { Fragment } from "react";
+import {Fragment, useEffect, useState} from "react";
 import NavBar from "../NavBar";
 import {Container} from "@mui/material";
+import EventService from "src/services/eventService";
+import {Event} from "src/interfaces/event"
 interface propsLayout {
   children: any;
 }
 const Layout = ({ children }: propsLayout) => {
+    const [events, setEvents] = useState<Event[]>([]);
+    const getEvents = async ()=>{
+        let response =await EventService.getEventsMock();
+        setEvents(response)
+    }
+    useEffect(() => {
+        getEvents()
+    }, []);
+
   return (
     <Fragment>
       <header>
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+const CONFIG = {
+    SERVICES: {
+        EVENT_SERVICE:{
+            URL:"",
+            API_KEY:"",
+        }
+    }
+};
+
+export default CONFIG
